52d0b9b68db11af1b168535937ee1093851ffca9
Shathik131 <sathiksathik126@gmail.com>
2024-03-04 20:23:19 +0530

    create and run the server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import Jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+// const PORT = process.env.PORT;
+const PORT = 3000; 
+
+app.listen(PORT, () => {
+  console.log(`server is running on the port: ${PORT}`);
+});
