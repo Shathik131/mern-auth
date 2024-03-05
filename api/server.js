4f9bff78ca240e89909f5695df226cf5ef3fb219
Shathik131 <sathiksathik126@gmail.com>
2024-03-05 09:33:58 +0530

    connect to db

--- api/server.js
+++ api/server.js
@@ -4,12 +4,30 @@ import cors from "cors";
 import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+// database start
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("once", () => {
+  console.log("database not connected");
+});
+
+db.once("open", () => {
+  console.log("database connected successfully");
+});
+
 const app = express();
+
+// server listening
+
+const PORT = 3000;
 // const PORT = process.env.PORT;
-const PORT = 3000; 
 
 app.listen(PORT, () => {
   console.log(`server is running on the port: ${PORT}`);
