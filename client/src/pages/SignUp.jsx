f58371d7c8eeb7a3013efcc867d31848ba9132b8
Shathik131 <sathiksathik126@gmail.com>
2024-03-05 15:43:37 +0530

    completed sign up page function

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      await axios
+        .post("/api/auth/sign-up", formData)
+        .then((res) => {
+          // console.log(res.data);
+          setFormData(res.data);
+          toast("Data Added Success", { type: "success", autoClose: 1500 });
+        })
+        .catch((err) => {
+          console.log(err);
+          toast("Data Added Failed", { type: "error", autoClose: 1500 });
+        });
+
+      setLoading(false);
+      setError(false);
+    } catch (err) {
+      res.send(err);
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1
@@ -11,12 +49,13 @@ const SignUp = () => {
         SignUp
       </h1>
 
-      <form action="" className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="User Name"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <input
@@ -24,6 +63,7 @@ const SignUp = () => {
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <input
@@ -31,13 +71,15 @@ const SignUp = () => {
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <button
+          disabled={loading}
           className="bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -47,6 +89,9 @@ const SignUp = () => {
           <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
+
+      <p className="text-red-700 pt-5">{error && "Something error!"}</p>
+      <ToastContainer />
     </div>
   );
 };
